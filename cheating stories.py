6409a7e62e777a9806400c93fb613d7cf166fd66
Shahid Mehmood <61820501+shahidmehmood57@users.noreply.github.com>
2025-06-25 21:15:43 +0500

    Update cheating stories.py

--- cheating stories.py
+++ cheating stories.py
@@ -3,7 +3,7 @@ import requests
 from datetime import datetime, timedelta
 
 # YouTube API Key
-API_KEY = "AIzaSyAD0vrIFI-Ta0WnYD7U9749u7u18wQu_U8"
+API_KEY = "AIzaSyAcP7wYgr2MSySie2uOUHt887AI_kYXlZI"
 YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
 YOUTUBE_VIDEO_URL = "https://www.googleapis.com/youtube/v3/videos"
 YOUTUBE_CHANNEL_URL = "https://www.googleapis.com/youtube/v3/channels"
